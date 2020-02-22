cbf26f7abd7fdf8bb978cfdec2b04407026aa4d5
rstatz <rmstatz@gmail.com>
2020-02-22 22:55:51 +0000

    cleanup

--- bluetooth_up.sh
+++
@@ -1,8 +0,0 @@
-sed -i '/^ExecStart=/ s/$/ -E/' /lib/systemd/system/bluetooth.service 
-# restart bluetoothd
-systemctl daemon-reload
-systemctl restart bluetooth
-# attach HCI UART
-dev=$(basename $(ls -d /sys/bus/usb/drivers/ftdi_sio/*/ttyUSB*))
-modprobe hci_uart
-btattach -B /dev/$dev -S 1000000 -P h4 &
